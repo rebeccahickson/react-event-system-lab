962b28ff446128dc9d573ac9b8ef289c5056cc20
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-25 13:04:30 -0500

    commit tree b2519a134968bc027a210cbeef3ba96756241f3f

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+export default class EyesOnMe extends React.Component {
+  render() {
+    return (
+      <button
+        onFocus={() => {
+          console.log("Good!");
+        }}
+        onBlur={() => {
+          console.log("Hey! Eyes on me!");
+        }}
+      />
+    );
+  }
+}
